c35c10fc8ae048bcafd143817a54777d5984eb13
reharsh67 <reharshdshpande@gmail.com>
2020-05-08 09:20:44 +0530

    Upated Ion Files

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_tissot.mk
+    $(LOCAL_DIR)/ion_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_tissot-user \
-    derp_tissot-userdebug \
-    derp_tissot-eng \
+    ion_tissot-user \
+    ion_tissot-userdebug \
+    ion_tissot-eng \
