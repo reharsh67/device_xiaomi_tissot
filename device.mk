bab49cc9170c115a4d6ca46bad94a87e2ecb2249
Dobsgw <38656887+Dobsgw@users.noreply.github.com>
2020-05-25 11:13:03 +0000

    tissot: Add Gcam Go

--- device.mk
+++ device.mk
@@ -67,8 +67,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8953 \
-    libmm-qcamera \
-    Snap
+    libmm-qcamera
 
 # ConsumerIr
 PRODUCT_PACKAGES += \
@@ -115,4 +114,7 @@ PRODUCT_PACKAGES += \
     Via
 
 
+# GCam Go
+PRODUCT_PACKAGES += \
+    GCam
 
